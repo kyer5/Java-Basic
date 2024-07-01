459f0bb4cb84e677801d36ecb1b3756236f444fb
강예린 <jjykhs0505@naver.com>
2024-07-01 15:21:19 +0900

    section 5. 패키지 - 시작

--- src/pack/PackageMain1.java
+++ src/pack/PackageMain1.java
@@ -1,6 +1,8 @@
 package pack;
 
 public class PackageMain1 {
-    Data data = new Data();
-    pack.a.User user = new pack.a.User();
+    public static void main(String[] args) {
+        Data data = new Data();
+        pack.a.User user = new pack.a.User();
+    }
 }
