3ef752f2c45b09f88d306afc7380551ebd4649ff
강예린 <jjykhs0505@naver.com>
2024-07-11 18:30:45 +0900

    section 12. 문제 풀이3

--- src/poly/ex/pay1/PayMain0.java
+++ src/poly/ex/pay1/PayMain0.java
@@ -1,20 +1,25 @@
 package poly.ex.pay1;
 
+import java.util.Scanner;
+
 public class PayMain0 {
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         PayService payService = new PayService();
 
-        String payOption1 = "kakao";
-        int amount1 = 5000;
-        payService.processPay(payOption1, amount1);
+        while (true) {
+            System.out.print("결제 수단을 입력하세요: ");
+            String payOption = sc.nextLine();
+            if (payOption.equals("exit")) {
+                System.out.println("프로그램을 종료합니다.");
+                return;
+            }
+            System.out.print("결제 금액을 입력하세요: ");
+            int amount = sc.nextInt();
+            payService.processPay(payOption, amount);
+            sc.nextLine();
+        }
 
-        String payOption2 = "naver";
-        int amount2 = 10000;
-        payService.processPay(payOption2, amount2);
-
-        String payOption3 = "bad";
-        int amount3 = 15000;
-        payService.processPay(payOption3, amount3);
     }
 }
