bc125747289a05ba0a70368cf7779af00fc7a44a
강예린 <jjykhs0505@naver.com>
2024-07-11 18:45:31 +0900

    section 12. 문제 풀이3

--- src/poly/ex/pay1/PayMain1.java
+++ src/poly/ex/pay1/PayMain1.java
@@ -2,7 +2,7 @@ package poly.ex.pay1;
 
 import java.util.Scanner;
 
-public class PayMain0 {
+public class PayMain1 {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
